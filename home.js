5218daa583100ccdb86ec6704e5dc9423b4fb3d6
NokibReja <nokibreja414@gmail.com>
2025-08-29 21:18:21 +0600

    responsive

--- home.js
+++ home.js
@@ -12,22 +12,61 @@ for (const heart of hearts) {
 const calls = document.getElementsByClassName('call-btn');
 for (const call of calls) {
     call.addEventListener('click', function () {
+
+        const contactCard = call.parentNode.parentNode;
+        const callTitle = contactCard.children[2].innerText;
+        const callNumber = contactCard.children[3].innerText;
+
         const coin = parseInt(document.getElementById('coin').innerText);
         if (coin < 20) {
-            alert('You do not have enough coin');
+            alert('You do not have enough coin. To make a call minimum 20 coin');
             return;
         }
+        alert(`Calling ${callTitle} ${callNumber}...`);
         const newCoin = coin - 20;
         document.getElementById('coin').innerText = newCoin;
+
+        // history added
+        const currentTime = new Date().toLocaleTimeString();
+        const historyClear = document.getElementById('call-history-container');
+        
+        // history card
+        const newCard = document.createElement("div");
+        newCard.innerHTML = `
+                <div class="mx-auto my-2 bg-[#F2F2F2] rounded-xl flex justify-between items-center flex-row p-3">
+                    <div class="">
+                        <h2 class="font-semibold text-base mt-2">${callTitle}</h2>
+                        <p class="py-2 text-[#5C5C5C]">${callNumber}</p>
+                    </div>
+                    <div>
+                        <p class="text-sm text-[#5C5C5C]">${currentTime}</p>
+                    </div>
+                </div>
+            `;
+
+        historyClear.append(newCard);
     })
 }
 
 // copy button feature 
 const copys = document.getElementsByClassName('copy-btn');
-for(const copy of copys){
-    copy.addEventListener('click', function(){
-        const copyCount = parseInt(document.getElementById('copy-count').innerText);
-        const newCopyCount = copyCount+1;
-        document.getElementById('copy-count').innerText = newCopyCount; 
+for (const copy of copys) {
+    copy.addEventListener('click', function () {
+        const numberToCopy = copy.parentNode.parentNode.children[3].innerText;
+
+        // number copy and count
+        navigator.clipboard.writeText(numberToCopy).then(() => {
+            alert(`Number has copied: ${numberToCopy}`)
+            const copyCount = parseInt(document.getElementById('copy-count').innerText);
+            const newCopyCount = copyCount + 1;
+            document.getElementById('copy-count').innerText = newCopyCount;
+        })
     })
 }
+
+// Call History Clear
+document.getElementById('btn-clear')
+    .addEventListener('click', function () {
+        const callHistoryClear = document.getElementById('call-history-container');
+        callHistoryClear.innerHTML = "";
+    })
